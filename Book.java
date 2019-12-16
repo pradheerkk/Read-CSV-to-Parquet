4426251c3cf931c4cb719cb6f674a3fe91ec5e57
pradheerkk <pradheerkk@gmail.com>
2019-12-16 13:07:45 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,16 @@
+import com.opencsv.bean.CsvBindByPosition;
+
+public class Turbine {
+	
+	@CsvBindByPosition(position = 0)
+	private String Name;
+  
+  @CsvBindByPosition(position = 1)
+	private String Author_name;
+	
+	@CsvBindByPosition(position = 2)
+	private String Cost;
+	
+  //setters and getters
+  
+  }
